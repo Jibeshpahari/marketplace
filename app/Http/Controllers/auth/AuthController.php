169e590862eac27ce4345b57782f803e99c7b2de
Jibesh Pahari <mailjibesh@webgrity.net>
2026-05-25 19:12:03 +0530

    login-part-1

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -11,8 +12,18 @@ class AuthController extends Controller
         return view('admin.login');
     }
 
-    public function adminLoginPost(Request $request){
-        dd($request->input());
+    public function adminLoginPost(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (Auth::attempt($credentials, $request->remember)) {
+
+            $request->session()->regenerate();
+
+            return redirect()->route('admin.dashboard');
+        }
+
+        return back()->with('error', 'Invalid credentials');
     }
 
 }
